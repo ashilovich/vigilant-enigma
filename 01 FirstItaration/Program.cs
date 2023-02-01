12eedd9cafc27ed06713abe8d4199b3c99f50554
Andrey <shilovish_AV@tut.by>
2023-02-01 21:02:28 +0300

    Моя задача

---
+++ 01 FirstItaration/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using _01_FirstItaration;
+
+namespace _01_FirstItaration
+{
+
+    class Greeting
+    {
+        public static void Hello()
+        {
+
+            Console.WriteLine("Вас приветствует программа по расчету ремонта квартиры");
+            Console.WriteLine("Просим протестировать нашу программу и оценить ее возможности");
+            Console.WriteLine("Это первая версия программы, которая рассчитает вам стоимость окраски стены");
+            Console.WriteLine("                                                          ");
+        }
+
+        public static void Goodbye()
+        {
+
+            Console.WriteLine("Пришла пора прощаться мой дорогой друг");
+            Console.WriteLine("Будем рады снова вас увидеть на нашем сайте");
+            Console.WriteLine("О своих эмоциях просим написать на email: bsuit2209@gmail.com");
+        }
+        
+
+
+
+    }
+
+
+
+    class InitialData
+    {
+        public static double lengthWall;
+
+        public static double widthWall;
+
+        public static double height;
+
+        public static double dimensionWall;
+
+        public static double AreaWall()
+        {
+         
+        Console.Write("Введите ширину вашей комнаты по оси X в метрах: ");
+        lengthWall = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите длину вашей комнаты по оси Y в метрах: ");
+        widthWall = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите высоту вашей комнаты в метрах: ");
+        height = Convert.ToDouble(Console.ReadLine());
+
+        dimensionWall= (lengthWall + widthWall )*2* height;
+        Console.WriteLine("Площадь вашей стены в м2: " + InitialData.dimensionWall+" "+"м2");
+        Console.WriteLine("                                                          ");
+
+        return dimensionWall;
+       
+
+        }
+    }
+
+    class Calculation
+    {
+        public static double priceWork = 5.0;
+
+        public static double priceMaterial=7.0;
+
+        public static double costMatWork;
+
+
+
+
+        public static double CostRoom()
+        {
+
+            Console.WriteLine("А теперь мы рассчитаем стоимость ремонта вашей комнаты");
+
+
+            costMatWork = InitialData.dimensionWall * (priceWork+ priceMaterial);
+            Console.WriteLine("Стоимость работ и материалов для окраски вашей стены составит: " + Calculation.costMatWork+" "+"бел.руб");
+            Console.WriteLine("                                                          ");
+            
+            return costMatWork;
+          
+
+        }
+
+    }
+
+
+    class Program
+    {
+       
+
+        static void Main()
+        {
+                       
+
+            Greeting.Hello();
+            InitialData.AreaWall();
+            Calculation.CostRoom();
+            Greeting.Goodbye();
+
+        }
+    }
+}
